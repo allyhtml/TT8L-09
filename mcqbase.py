36748d11b9ad5a883f040ac7961cb7e37ac47afc
Amira Aishah <amraishah11@gmail.com>
2024-05-10 14:52:59 +0800

    font

--- mcqbase.py
+++ mcqbase.py
@@ -6,3 +6,12 @@ class QuizGame:
     def __init__(self, root):
         self.root = root
         self.root.title("MCQ Quiz Game")
+
+        custom_font = font.Font(family="Helvetica", size=14)
+
+        self.question = [
+            {
+                "question": "1) What is the primary function of an adder circuit?"
+                "option": 
+            }
+        ]
