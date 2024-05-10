2057a82e28c0cdda4de25e3142a43508b6403948
Amira Aishah <amraishah11@gmail.com>
2024-05-10 14:47:45 +0800

    self root

--- mcqbase.py
+++ mcqbase.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import font 
-hahahahahahaha
-hehehehehe
-hhohohhoho
+from random import shuffle
+
+class QuizGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("MCQ Quiz Game")
