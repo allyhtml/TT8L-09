a8c99e3b9d42d28928a54dccdbdf006869c89335
Amira Aishah <amraishah11@gmail.com>
2024-05-12 00:47:03 +0800

    question

--- mcqbase.py
+++ mcqbase.py
@@ -11,7 +11,22 @@ class QuizGame:
 
         self.question = [
             {
-                "question": "1) What is the primary function of an adder circuit?"
-                "option": 
+                "question": "1) What is the primary function of an adder circuit?",
+                "options": ["Logical manipulation", "Electric transmission", "Addition of digits", "Multiplication of digits"],
+                "answer": "Addition of digits"
+            },
+            {
+                "question": "2) Which component of the CPU extensively uses adders?",
+                "options": ["GPU", "ALU", "RAM", "Cache"],
+                "answer": "ALU"
+            },
+            {
+                "question": "3) What type of addition do the majority of adders handle?",
+                "options": ["Octal addition", "Decimal addition", "Hexadecimal addition", "Binary addition"],
+                "answer": "Binary addition"
             }
         ]
+
+root = tk.Tk()
+app = QuizGame(root)
+root.mainloop()
